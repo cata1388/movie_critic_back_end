78b9767e493b1be7a80b28c8a4144390a20d92b3
catalina <cata.1388@gmail.com>
2013-11-16 18:51:50 -0500

    back end validaciones

--- app/models/calificacion.rb
+++ app/models/calificacion.rb
@@ -1,4 +1,5 @@
 class Calificacion < ActiveRecord::Base
+	validates_presence_of :usuario_id, :pelicula_id, :calificacion, :message => "Campos requeridos"
 	belongs_to :usuario
 	belongs_to :peliula
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,3 +1,5 @@
 class Usuario < ActiveRecord::Base
+	validates_presence_of :correo, :nombre, :contrasenya, :message => "Campos requeridos"
+	validates_uniqueness_of :correo, :message => "Correo ya existente"
 	has_many :calificacions, dependent: :destroy
 end
